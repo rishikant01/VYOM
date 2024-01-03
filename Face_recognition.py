948778d42887b77ba786dd0b023373bad54c364c
Rishi Kant Shukla <97825285+rishikant01@users.noreply.github.com>
2024-01-03 21:22:04 +0530

    Update Face_recognition.py
    Made changes to use this face recognition for the face unlock system of my bot.

--- Face_recognition.py
+++ Face_recognition.py
@@ -1,57 +1,79 @@
 import cv2
-recognizer = cv2.face.LBPHFaceRecognizer_create()
-recognizer.read(r'C:\Users\HP\Desktop\VYOM\Face_detect\Trainer.yml')
-cascadePath="haarcascade_frontalface_default.xml"
-faceCascade=cv2.CascadeClassifier(cascadePath)
+from Listen_Speak import Speak_en
 
-font = cv2.FONT_HERSHEY_SIMPLEX
+def face_recognition():
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+    recognizer.read(r'C:\Users\HP\Desktop\VYOM\Face_detect\Trainer.yml')
+    cascadePath = "Face_detect\haarcascade_frontalface_default.xml"
+    faceCascade = cv2.CascadeClassifier(cascadePath)
 
-id=2
+    font = cv2.FONT_HERSHEY_SIMPLEX
 
-names=['','Rishi'] #leaving first space empty because counter starts from 0
+    id = 2
 
-cam=cv2.VideoCapture(0,cv2.CAP_DSHOW) #DSHOW to remove warning
-cam.set(3,1280)
-cam.set(4,720)
+    names = ['', 'Rishi']  # leaving first space empty because counter starts from 0
 
-#Defining minimum window side to be recognizeed as a face
-minW=0.1*cam.get(3)
-minH=0.1*cam.get(4)
-flag=0
-while True:
-    ret,img=cam.read() #read the frames using the above created object
-    converted_image=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # DSHOW to remove warning
+    cam.set(3, 1280)
+    cam.set(4, 720)
 
-    faces = faceCascade.detectMultiScale(
-        converted_image,
-        scaleFactor=1.2,
-        minNeighbors=5,
-        minSize=(int(minW),int(minH))
-    )
+    # Defining minimum window side to be recognized as a face
+    minW = 0.1 * cam.get(3)
+    minH = 0.1 * cam.get(4)
+    flag = 0
+    accuracy=0
+    x, y, w, h = 0, 0, 0, 0
 
-    for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-        label, accuracy = recognizer.predict(converted_image[y:y+h,x:x+w])
+    while True:
+        ret, img = cam.read()  # read the frames using the above created object
+        converted_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        #checking accuracy
-        if accuracy < 100:
-            recognized_name = names[label]
-            if recognized_name=='Rishi':
-                flag=1
-            #accuracy_str = "  {0}% Match Found".format(round(100 - accuracy))
+        faces = faceCascade.detectMultiScale(
+            converted_image,
+            scaleFactor=1.2,
+            minNeighbors=5,
+            minSize=(int(minW), int(minH))
+        )
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            label, accuracy = recognizer.predict(converted_image[y:y + h, x:x + w])
+
+            # checking accuracy
+            if accuracy < 100:
+                recognized_name = names[label]
+                accuracy_str = "  {0}% Match Found".format(round(100 - accuracy))
+                if recognized_name == 'Rishi':
+                    flag = 1
+                    #Speak_en("Welcome Sir!")
+                    break  # Break out of the loop once face is recognized
         else:
+            # This block will execute if the for loop completes without breaking
             recognized_name = "unknown"
             accuracy_str = "  {0}% Match Found".format(round(100 - accuracy))
+            #Speak_en("No match found!")
 
-        cv2.putText(img, str(recognized_name), (x+5, y-5), font, 1, (255, 255, 255), 2)
-        cv2.putText(img, str(accuracy_str), (x+5, y+h-5), font, 1, (255, 255, 0), 1)
+        cv2.putText(img, str(recognized_name), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
+        cv2.putText(img, str(accuracy_str), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
+
+        cv2.imshow('camera', img)
+
+        k = cv2.waitKey(10) & 0xff
+        if k == 27 or flag == 1:
+            break
 
 
-    cv2.imshow('camera',img)
+    cam.release()
+    cv2.destroyAllWindows()
 
-    k= cv2.waitKey(10) & 0xff
-    if k==27:
-        break
-cam.release()
-cv2.destroyAllWindows()  
-    
+    return flag
+
+def face_unlock():
+    if face_recognition():
+        return 1
+    else:
+        return 0
+
+# Uncomment the line below to test the Face_unlock function
+# result = face_unlock()
+# print(result)
